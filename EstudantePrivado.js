7b5a6b5f51c26e820a4ee05e2b7ab66cf4a37f15
unknown <Inovape@CBRANB155827.ifb.local>
2026-08-04 16:59:58 -0300

    Encapsulamento

---
+++ EstudantePrivado.js
@@ -0,0 +1,51 @@
+class EstudantePrivado {
+    //Declarar campos privados no topo da classe
+    #nome
+    #matricula
+    #notas
+
+    constructor(nome, matricula) {
+        //Usar # para se referir aos campos privados
+        this.#nome = nome
+        this.#matricula = matricula
+        this.#notas = []
+    }
+
+    //getters permitem apenas leitura
+    get nome() {
+        return this.#nome
+    }
+
+    //setter para o nome (permite alteração direta do valor)
+    set nome(novoNome) {
+        //poderia ter alguma validação para proteger melhor o atributo
+        //esse setter simplesmente atribui o novo valor ao atributo privado
+        this.#nome = novoNome
+    }
+
+    get matricula() {
+        return this.#matricula
+    }
+
+    get notas() {
+        return [...this.#notas]
+        //retorna e copia o array usando Sintaxe de Espalhamento
+        //"pega" todos os elementos de um array ou propriedade de um objet0
+    }
+    
+    //métodos públicos que manipulam o array privado
+    adicionarNota(nota) {
+        if (nota >= 0 && nota <=10){
+            this.#notas.push(nota)
+        } else {
+            console.log(`Erro: Nota ${nota} é inválida`)
+        }
+    }
+
+    calcularMedia() {
+        if (this.#notas.length === 0) return 0
+        const soma = this.#notas.reduce((acc, n) => acc + n, 0)
+        return (soma / this.#notas.length).toFixed(2)
+    }
+
+}
